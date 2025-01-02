4b1e4b4cb41e66a53f87417f01a2774a9f79156f
Johannes Barthold <valentin.j.schmidt@web.de>
2025-01-02 21:53:38 +0100

    trying to implement Linear effects model , but something is wrong with the distances

--- src/linear_effects_model.py
+++ src/linear_effects_model.py
@@ -1,83 +1,37 @@
 import pandas as pd
-import statsmodels.api as sm
 import statsmodels.formula.api as smf
-import csv
+import matplotlib.pyplot as plt
+
 
 # Load the data
-votes = pd.read_csv("data/votes.csv")
-distances = pd.read_csv("data/distances.csv")
+votes = pd.read_csv("data/vote_distances.csv")
+votes = votes.dropna()
+votes["round"] = votes["round"].astype("category")
 
-# read csv file to a list of dictionaries
-with open("data/country_info.csv", "r") as file:
-    csv_reader = csv.DictReader(file)
-    country_info = [row for row in csv_reader]
-
-
-print(votes.head())
-print(distances.head())
-print(country_info)
-
-# Helper functions
-
-
-def get_country_capital(code_to_find, countries):
-    return next(
-        (
-            country["capital"]
-            for country in countries
-            if country["code"] == code_to_find
-        ),
-        None,
-    )
-
-
-import pandas as pd
-import csv
-
-
-def transform_distances_to_codes(distances, country_info_file):
-    """
-    Transforms a distances CSV with capitals as labels to one with country codes as labels.
-
-    Args:
-        distances_file (str): Path to the CSV file with distances (capitals as labels).
-        country_info_file (str): Path to the CSV file mapping capitals to country codes.
-        output_file (str, optional): Path to save the transformed CSV. If None, it won't save.
-
-    Returns:
-        pd.DataFrame: Transformed DataFrame with country codes as labels.
-    """
-    # Load the distances and country info
-
-    # Read country info into a dictionary for quick lookups
-    with open(country_info_file, "r") as file:
-        csv_reader = csv.DictReader(file)
-        capital_to_code = {row["capital"]: row["code"] for row in csv_reader}
-
-    distances.rename(
-        columns=lambda col: capital_to_code.get(
-            col, col
-        ),  # Replace capital with code in columns
-        inplace=True,
-    )
-
-    distances["name"] = distances["name"].apply(lambda x: capital_to_code.get(x, x))
-
-    return distances
-
-
-# Transform distances
-transformed_distances = transform_distances_to_codes(distances, "data/country_info.csv")
-
-print(transformed_distances.head())
-
-## data preparation/ exploration
-
+votes = votes[votes["distance"] > 0]
+votes = votes[votes["round"] == "final"]
+# votes = votes[votes["year"] == 2023]
+# votes = votes[votes["from_country"] == "de"]
+# votes["year"] = votes["year"].astype("category")
+votes["distance"] = votes["distance"].astype("float")
 
 ## Linear Effects Model
 print(votes.head())
-smf.mixedlm(
-    formula="total_votes ~ distance + year", data=votes, groups=votes["from_country_id"]
-).fit().summary()
+mixed_model = smf.mixedlm(
+    formula="total_points ~ distance",
+    data=votes,
+    groups=votes["year"],
+).fit()
+print(mixed_model.summary())
 
 # Check the assumptions of the linear effects model
+votes["residuals"] = mixed_model.resid
+votes["fitted_values"] = mixed_model.fittedvalues
+
+# Plot the residuals
+plt.scatter(votes["fitted_values"], votes["residuals"])
+plt.xlabel("Fitted values")
+plt.ylabel("Residuals")
+plt.title("Residuals vs. Fitted values")
+plt.axhline(y=0, color="black", linestyle="--")
+plt.show()
